c873a7742ab2b01c54bfbc4379bd554f56576f81
jf-759 <jane.fernandez@students.dominican.edu>
2025-09-26 17:30:55 -0700

    testing data

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const deck = {
+    title: "Trainer's Trial",
+    description: "Flip the card to reveal the creature! Test your memory and see how many you know!"
+    cards: [
+        {
+            id: 1,
+            name: "Pikachu",
+            image: ""
+            description: "An electric-type Pokemon known for storing electricity in its cheeks."
+        },
+    ]
+}
+
+export default deck;
